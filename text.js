8f09e9340005b98406dcc554d7670b438efd7d62
hoanggh123 <96602378+hoanggh123@users.noreply.github.com>
2022-07-12 18:22:09 +0700

    first commit

---
+++ text.js
@@ -0,0 +1,13 @@
+//P1: Gioi thieu
+//B1: Lời khuyên trước khóa học
+//B2: Cài đặt môi trường
+//P2: Biến,coments,bult-in
+//B3: Sử dụng JavaScript với HTML
+//B4: Làm quen với màn thử thách
+//B5: Thử thách đầu tiên
+alert("Hello world")
+//B6: Lưu ý khi học lập trình tại F8
+//B7: Khái niệm biến và cách sử dụng biến
+//B8: Thực hành sử dụng biến 
+var weght = 11;
+alert("Can nặng của tui là"+ weght)
