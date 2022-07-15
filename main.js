a940e47865f0adb1e26f90d97f8f4b6a4d85ff5b
hoanggh123 <96602378+hoanggh123@users.noreply.github.com>
2022-07-15 19:40:37 +0700

    update

--- main.js
+++ main.js
@@ -3,27 +3,30 @@ function dangnhap() {
         document.getElementById("form").style.position = 'fixed';
         document.getElementById("auth-form--register").style.display = 'none';
         document.getElementById("auth-form--login").style.display = 'block';
-        document.getElementById("auth-form--login").style.animation = 'fadeIn linear 0.5s';
-        
-        
+        document.getElementById("auth-form--login").style.animation = 'fadeIn linear 0.5s';    
     };
 function dangki() {
-        
         document.getElementById("form").style.position = 'fixed';
         document.getElementById("auth-form--register").style.display = 'block';
         document.getElementById("auth-form--login").style.display = 'none';  
-        document.getElementById("auth-form--register").style.animation = 'fadeIn linear 0.5s'; 
-    
+        document.getElementById("auth-form--register").style.animation = 'fadeIn linear 0.5s';   
 };
 function exitlogin() {
     document.getElementById("auth-form--login").style.display = 'none';
-    document.getElementById("form").style.position = '';
-
-    
+    document.getElementById("form").style.position = ''; 
 }
 function exitregister() {
     document.getElementById("auth-form--register").style.display = 'none';
-    document.getElementById("form").style.position = '';
+    document.getElementById("form").style.position = '';   
+};
+function remove() {
+    document.getElementById("header_cart-list-item_remove").style.display = 'none';
+    document.getElementById("header_cart-no-cart-img").style.display = 'block';
+    document.getElementById("header_cart-no-cart-img").style.textAlign = 'center';
+    document.getElementById("header_cart-heading").style.display = 'none';
+    document.getElementById("header_cart-list-no-cart-msg").style.display = 'block';
+}
 
     
-}
+
+
