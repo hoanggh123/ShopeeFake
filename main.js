f6a96921ebbfb0528060b13b9620bb83dee7be34
hoanggh123 <96602378+hoanggh123@users.noreply.github.com>
2022-07-23 21:32:37 +0700

    Update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function dangnhap() {
-    
         document.getElementById("form").style.position = 'fixed';
         document.getElementById("auth-form--register").style.display = 'none';
         document.getElementById("auth-form--login").style.display = 'block';
@@ -27,8 +26,29 @@ function exitregister() {
 //     document.getElementById("header_cart-list-no-cart-msg").style.display = 'block';
 // }
 function link() {
+    var categoryitemlinkchildren = document.getElementById("category-item-children");
+    var categoryitemlinkmale = document.getElementById("category-item-male");
+    var categoryitemlinkfemale = document.getElementById("category-item-female");
+    categoryitemlinkmale.classList.add('category-item-active');
+    categoryitemlinkfemale.classList.remove('category-item-active');
+    categoryitemlinkchildren.classList.remove('category-item-active');
     document.getElementById("category-item-link-male").style.color = 'var(--primary-color)';
     document.getElementById("category-item-link-female").style.color = 'var(--text-color)';
+    document.getElementById("category-item-link-children").style.color = 'var(--text-color)';
+    
+   
+}
+function link2() {
+    var categoryitemlinkchildren = document.getElementById("category-item-children");
+    var categoryitemlinkmale = document.getElementById("category-item-male");
+    var categoryitemlinkfemale = document.getElementById("category-item-female");
+    categoryitemlinkmale.classList.remove('category-item-active');
+    categoryitemlinkchildren.classList.add('category-item-active');
+    categoryitemlinkfemale.classList.remove('category-item-active');
+
+    document.getElementById("category-item-link-male").style.color = 'var(--text-color)';
+    document.getElementById("category-item-link-female").style.color = 'var(--text-color)';
+    document.getElementById("category-item-link-children").style.color = 'var(--primary-color)';
 
    
 }
